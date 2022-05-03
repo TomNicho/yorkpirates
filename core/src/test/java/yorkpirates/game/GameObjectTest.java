9318e0fbf8d5ad70f8756575c5e1efb62c33a928
TomNicho <tomnicho20@gmail.com>
2022-05-03 22:28:09 +0100

    Added Test env

---
+++ core/src/test/java/yorkpirates/game/GameObjectTest.java
@@ -0,0 +1,14 @@
+package yorkpirates.game;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class GameObjectTest {
+
+    @Test
+    public void testStuff() {
+        System.out.println("Hello");
+        assertTrue(true);
+    }
+}
